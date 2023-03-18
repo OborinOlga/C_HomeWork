5c6fbf2157766525538ed21c75087759e43c8801
OborinOlga <125594075+OborinOlga@users.noreply.github.com>
2023-03-18 16:22:32 +0500

    Add Task 9

---
+++ Task09/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая выводит случайное число 
+// из отрезка [10, 99] и показывает наибольшую цифру числа
+
+int number = new Random ().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезки 10-99-> {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if ( firstDigit >= secondDigit) 
+{
+    Console.Write(firstDigit);
+}
+else 
+{
+    Console.WriteLine(secondDigit);
+}
