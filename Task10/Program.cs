6f495dd281a35f950593e121ebb6f0e40a9995b0
OborinOlga <125594075+OborinOlga@users.noreply.github.com>
2023-03-18 23:24:57 +0500

    Add Task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,16 +1,55 @@
-﻿// Напишите программу, которая вывоодит случайное трехзначное число
-// и удаляет вторую цифру этого числа
+﻿// Напишите программу, которая будет принимать на ввод двачисла и выводить,
+// является ли первое число кратным второму. Если число 1 не кратно числу 2, то
+// программа выводит остаток от деления.
 
-int number = new Random().Next(100, 1000);
-Console.WriteLine($"случайное число из отрезка от 100 до 999 -> {number}");
+Console.WriteLine("Введите первое целое число:  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число:  ");
+int number2 = Convert.ToInt32(Console.ReadLine());
 
-// int firstDigit = number / 100;
-// int thirdDigit = number % 10;
-// int newNumber = 10*firstDigit + thirdDigit;
-// Console.WriteLine(newNumber);
+// if (number1 >= number2)
+// {
+//     if (number1 % number2 == 0)
+//     {
+//         Console.WriteLine("Первое число кратно второму");
+//     }
+//     else
+//     {
+//         while (number1 > number2)
+//         {
+//             number1 = number1 - number2;
+//         }
+//         Console.WriteLine($"Остаток от деления = {number1}");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Первое число меньше второго");
+// }
 
-int FirstThirdDigit (int num)
-int firstDigit = number / 100;
-int thirdDigit = number % 10;
-int newNumber = 10*firstDigit + thirdDigit;
-int newNumber = 10*firstDigit + thirdDigit;
+// 2й способ
+// int result = number1%number2;
+// if (result==0)
+// {
+//      Console.WriteLine("Кратно");
+// }
+// else
+// {
+//      Console.WriteLine($"Не кратно, остаток от деления = {result}");
+// }
+
+// 3й способ и использованием методов
+int Multiple (int num1, int num2)
+{
+    return num1%num2;
+}
+
+int result = Multiple (number1, number2);
+if (result == 0)
+{
+    Console.WriteLine ("Кратно");
+}
+else
+{
+    Console.WriteLine ($"Не кратно, остаток от деления = {result}");
+}
