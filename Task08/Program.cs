9f10f507dde8a32fbf8ab5bd9410f29b32b52d96
OborinOlga <125594075+OborinOlga@users.noreply.github.com>
2023-03-18 10:11:16 +0500

    Add Example 08

---
+++ Task08/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if(number > 1)
+{
+while(count <= number)
+{
+    Console.Write($"{count} ");
+    count = count+2;
+}
+}
+else
+{
+    Console.WriteLine("Нет таких чисел");
+}
