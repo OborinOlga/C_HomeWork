15e95ada6e9466c9ea27d0420b4d3708f71a9af3
OborinOlga <125594075+OborinOlga@users.noreply.github.com>
2023-03-18 09:16:48 +0500

    Add Example 02

---
+++ Task02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое натуральное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.WriteLine($"Наибольшее число = {number1}");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"Наибольшее число = {number2}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
