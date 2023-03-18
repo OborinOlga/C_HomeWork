82821d358b4c50d0bae60c47bf8fbc7bcb45deca
OborinOlga <125594075+OborinOlga@users.noreply.github.com>
2023-03-18 09:59:14 +0500

    Add Example 06

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -4,7 +4,7 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе натуральное число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-if(number1 > number2)
+if (number1 > number2)
 {
     Console.WriteLine($"Наибольшее число = {number1}");
 }
---
+++ Task06/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое натуральное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третее натуральное число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 >= number2)
+{
+    if (number1 >= number3)
+    {
+        Console.WriteLine($"Наибольшее число = {number1}");
+    }
+    else
+    {
+        Console.WriteLine($"Наибольшее число = {number3}");
+    }
+}
+else if (number2 >= number3)
+{
+     Console.WriteLine($"Наибольшее число = {number2}");
+}
+else
+{
+     Console.WriteLine($"Наибольшее число = {number3}");
+}
+
+
