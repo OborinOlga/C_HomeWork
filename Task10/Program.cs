c2d7452689f36ab028841a5abfa6e57f9a402762
OborinOlga <125594075+OborinOlga@users.noreply.github.com>
2023-03-18 21:47:01 +0500

    Add Task 11

---
+++ Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая вывоодит случайное трехзначное число
+// и удаляет вторую цифру этого числа
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"случайное число из отрезка от 100 до 999 -> {number}");
+
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int newNumber = 10*firstDigit + thirdDigit;
+// Console.WriteLine(newNumber);
+
+int FirstThirdDigit (int num)
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+int newNumber = 10*firstDigit + thirdDigit;
+int newNumber = 10*firstDigit + thirdDigit;
---
+++ Task11/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая вывоодит случайное трехзначное число
+// и удаляет вторую цифру этого числа
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"случайное число из отрезка от 100 до 999 -> {number}");
+
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int newNumber = 10*firstDigit + thirdDigit;
+// Console.WriteLine(newNumber);
+
+int FirstThirdDigit (int num)
+{
+int firstDigit = num / 100;
+int thirdDigit = num % 10;
+int newNum = 10*firstDigit + thirdDigit;
+return newNum;
+}
+
+int newNumber = FirstThirdDigit (number);
+Console.WriteLine(newNumber);
