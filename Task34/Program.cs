4941557fd420bceac20b5e87bed8c4464ffa4f70
OborinOlga <125594075+OborinOlga@users.noreply.github.com>
2023-04-06 02:16:08 +0500

    Add Task 46

---
+++ Task34/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+// ВВод числа с экрана
+int Prompt (string message)
+{
+    Console.Write(message);
+    string ReadInput = System.Console.ReadLine();
+    int result = int.Parse(ReadInput);
+    return result;
+}
+
+// Метод заполнения массива
+int [] FillArray (int Length)
+{
+    int[] arr = new int[Length];
+    Random rand = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        arr[i] =rand.Next(100, 1000);
+    }
+    return arr;
+}
+
+// Метод вывода массива на экран
+void PrintArray (int[] arr)
+{   
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length-1; i++)
+    {
+        System.Console.Write($"{arr[i]}, ");
+    }
+    System.Console.Write($"{arr[arr.Length-1]}");
+    System.Console.WriteLine("]");
+}
+
+// Метод подсчета четных чисел в массиве
+int QuantityEvenNumber(int[] arr)
+{
+    for (int i= 0; i <= arr.Length-1; i++)  
+    {
+        int result=0;
+        if (arr[i]%2 ==0) result=result+1;
+        else result=result;
+    }
+}
+
+
+int length = Prompt("Введите длину массива: ");
+int[] array = FillArray(length);
+PrintArray(array);
+Console.WriteLine($"В данном массиве количество чётных чисел = {QuantityEvenNumber[array]}");
---
+++ Task46/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+
+
+// void FillArray(int[,] matr)
+// {
+//     for (int i=0; i<matr.GetLength(0); i++)
+//     {
+//         for (int j=0; j<matr.GetLength(1); j++)
+//         {
+//             matr[i,j]= new Random().Next(0, 10);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] matr)
+// {
+//     for (int i=0; i<matr.GetLength(0); i++)
+//     {
+//         for (int j=0; j<matr.GetLength(1); j++)
+//         {
+//             Console.Write($"{matr[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] matrix = new int [3,4];
+// FillArray(matrix);
+// PrintArray(matrix);
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($"{matrix[i, j], 5} ");
+            }
+            Console.WriteLine("|");
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2d);
