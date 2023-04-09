74ab4ccbe27496dc0e96001df65e8be03ddea37c
OborinOlga <125594075+OborinOlga@users.noreply.github.com>
2023-04-09 16:29:20 +0500

    Add Task 42

---
+++ Task42/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int[] ChangeToBinaryNumber (int num)
+{
+    int[] arr = new int[8];
+    if (num == 0) arr[arr.Length-1] = 0;
+    int i = 0;
+    while (num > 0)
+    {
+        if (num%2 ==0) arr[arr.Length-1-i] = 0;
+        else arr[arr.Length-1-i] = 1;
+        num = num/2;
+        i++;
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    for(int i=0; i <arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+    }
+}
+
+
+Console.WriteLine("Введите натуральное десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = ChangeToBinaryNumber(number);
+PrintArray (array);
