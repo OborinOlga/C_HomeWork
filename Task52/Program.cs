62a729d61b205e7ef7a533f7413231585a844598
OborinOlga <125594075+OborinOlga@users.noreply.github.com>
2023-04-09 23:33:06 +0500

    Add Task 52

---
+++ Task52/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($"{matrix[i, j], 5} ");
+            }
+            Console.WriteLine("|");
+    }
+}
+
+void AverageColumnsMatrix (int [,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum = sum + matrix[i, j];
+        }
+        double average = Math.Round(sum/matrix.GetLength(1), 2);
+        Console.WriteLine($"Среднее арифметическое элементов {j} -го столбца = {average}"); 
+    } 
+     
+}
+
+Console.Write("Введите размерность cтрок в массиве m: ");
+int mRows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размерность в массиве n: ");
+int nColumns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение элементов массива: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение элементов массива: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+int[,] array2d = CreateMatrixRndInt(mRows, nColumns, minValue, maxValue);
+PrintMatrix(array2d);
+AverageColumnsMatrix (array2d);
