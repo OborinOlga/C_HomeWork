14f0e64b4e3a60d1ba84a49217c9fb5dbd1e27f3
OborinOlga <125594075+OborinOlga@users.noreply.github.com>
2023-04-13 23:38:47 +0500

    Add Task 69

--- Task69/Program.cs
+++ Task69/Program.cs
@@ -2,4 +2,15 @@
 // в целую степень B с помощью рекурсии.
 // A = 3; B = 5 -> 243 (3⁵)
 // A = 2; B = 3 -> 8
-Console.WriteLine("Hello, World!");
+Console.Write("Введите натуральное число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write(DegreeDecreasingNumber(numberA, numberB));
+
+int DegreeDecreasingNumber(int A, int B)
+{
+    if (B == 1) return A;
+    else return checked (A * DegreeDecreasingNumber(A, B-1));
+}
+
